d59491d05e1693a073e031da97e7ffc7be4cd9ee
dataBio <46449724+zidude1234@users.noreply.github.com>
2022-02-01 14:43:13 -0500

    Create Day03 Syntax.py

---
+++ Day03/Python Syntax/Day03 Syntax.py
@@ -0,0 +1,21 @@
+#Day Three of the 30 Day Challenge
+# Introduction
+# Author: dataBio
+# Date: 0x Feb 2022
+# IDE: VSCode
+
+# import the "sys" module
+import sys
+# import the "os" module
+import os
+# import the "platform" module
+import platform
+# import the "platform" module
+import math
+
+def main():
+
+    os.system("cls") # clear console
+
+if __name__ == "__main__":
+  main()
