dbd9a096cad0bfee0ba615d96486d0668d5bd771
dataBio <46449724+zidude1234@users.noreply.github.com>
2022-02-04 02:22:50 -0500

    Add files via upload

---
+++ Day 04/Python Syntax/Day04 Syntax.py
@@ -0,0 +1,43 @@
+#Day Three of the 30 Day Challenge
+# Introduction
+# Author: dataBio
+# Date: 03 Feb 2022
+# IDE: VSCode
+
+# import the "sys" module
+import sys
+# import the "os" module
+import os
+# import the "platform" module
+import platform
+# import the "platform" module
+import math
+
+def main():
+
+    os.system("cls") # clear console
+
+#1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+  main()
