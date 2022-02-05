795729725fc8c13afe03c4a41be9e8fdf6dd5a11
dataBio <46449724+zidude1234@users.noreply.github.com>
2022-02-05 13:23:35 -0500

    Update Day05 Syntax.py

--- Day 05/Python Syntax/Day05 Syntax.py
+++ Day 05/Python Syntax/Day05 Syntax.py
@@ -17,8 +17,91 @@ import re
 
 def main():
 
-    os.system("cls") # clear console
+  os.system("cls") # clear console
+  #1
+  listOne = []
+  print("Number 1")
+  print(listOne)
+  print('\n')
+  
+  #2
+  listOne = ["Name","Age",3,465,"Country","City","Asabeneh",250,["Halo","Mario Bros", "Street Fighter"], ('Coding', 'For' , 'All')]
+  print("Number 2")
+  print(listOne)
+  print('\n')
 
+  #3 Find the length of your list
+  print("Number 3")
+  print(f'listOne Length :{len(listOne)}')
+  print('\n')
+  
+  #4 Get the first item, the middle item and the last item of the list
+  print("Number 4")
+  print(f'first item :{listOne[0]}')
+  print(f'last item :{listOne[len(listOne)-1]}')
+  middle_item = listOne[ceil(len(listOne)/2)]
+  print(f'Middle item with index ({listOne.index(middle_item)}) : {listOne[ceil(len(listOne)/2)]}')
+  print('\n')
+  
+  #5 Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+  mixed_data_types = list(("dataBio",99,"3.2m","Complicated","50 Git Street"))
+  print("Number 5")
+  print(mixed_data_types)
+  print('\n')
+
+
+  #6 Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+  it_companies = list(("Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle","Amazon"))
+  print("Number 6")
+  print("List Declared")
+  print('\n')
+
+  #7 Print this list
+  print("Number 7")
+  print(it_companies)
+  print('\n')
+
+  #8 Print the number of companies in the list
+  print("Number 8")
+  for item_i in it_companies:
+    print(f'\tCompany {(it_companies.index(item_i))+1}: {item_i}')
+  print('\n')
+
+  #9 Print the first, middle and last company
+  print("Number 9")
+  print(f'first company :{it_companies[0]}')
+  print(f'middle company :{it_companies[3]}')
+  print(f'last company :{it_companies[len(it_companies)-1]}')
+  print('\n')
+
+  #10 Print the list after modifying one of the companies
+  it_companies [0] = "Metaverse"
+  print("New List:", it_companies)
+  
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 if __name__ == "__main__":
   main()
