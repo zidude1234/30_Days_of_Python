8242e024c6e7c9b704afcf976fa32b515f2c079b
dataBio <46449724+zidude1234@users.noreply.github.com>
2022-02-05 10:48:13 -0500

    Create Day05 Syntax.py

---
+++ Day 05/Python Syntax/Day05 Syntax.py
@@ -0,0 +1,24 @@
+#Day Five of the 30 Day Challenge
+# Introduction
+# Author: dataBio
+# Date: 05 Feb 2022
+# IDE: VSCode
+
+# import the "sys" module
+import sys
+# import the "os" module
+import os
+# import the "platform" module
+import platform
+# import the "platform" module
+import math
+# import the "re" module
+import re
+
+def main():
+
+    os.system("cls") # clear console
+
+    
+if __name__ == "__main__":
+  main()
