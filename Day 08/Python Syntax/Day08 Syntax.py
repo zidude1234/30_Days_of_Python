7915545a5e75faf9a43ddf371b0bd14438672d1e
dataBio <46449724+zidude1234@users.noreply.github.com>
2022-02-07 09:20:53 -0500

    Create Day08 Syntax.py

---
+++ Day 08/Python Syntax/Day08 Syntax.py
@@ -0,0 +1,24 @@
+#Day Eight of the 30 Day Challenge
+# Introduction
+# Author: dataBio
+# Date: 07 Feb 2022
+# IDE: VSCode
+
+# import the "sys" module
+import sys
+# import the "os" module
+import os
+# import the "platform" module
+import platform
+# import the "platform" module
+import math
+# import the "re" module
+import re
+
+def main():
+
+    os.system("cls") # clear console
+
+    
+if __name__ == "__main__":
+  main()
