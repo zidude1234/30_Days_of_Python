9c54597e409389fcc93bcd19d83709ade270540b
dataBio <46449724+zidude1234@users.noreply.github.com>
2022-02-07 15:03:52 -0500

    Create Day09 Syntax.py

---
+++ Day 09/Python Syntax/Day09 Syntax.py
@@ -0,0 +1,34 @@
+#Day Nine of the 30 Day Challenge
+# Introduction
+# Author: dataBio
+# Date: 07 Feb 2022
+# IDE: VSCode
+
+# import the "sys" module
+import sys
+# import the "os" module
+import os
+# import the "platform" module
+import platform
+# import the "platform" module
+import math
+# import the "re" module
+import re
+
+def main():
+
+  s1 = 'Day 8 Exercise level 1'
+  s2 = 'Day 7 Exercise level 2'
+  s3 = 'Day 7 Exercise level 3'
+  s0 =  ''
+
+  for i in range(20):
+    s0 += "--"
+
+  s = s0 + s1 + s0
+  print(s)
+  os.system("cls") # clear console
+
+    
+if __name__ == "__main__":
+  main()
