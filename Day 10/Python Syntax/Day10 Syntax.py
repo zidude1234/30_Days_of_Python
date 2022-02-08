8b97984cdce402119701e24d7181e88b0ef8fe09
dataBio <46449724+zidude1234@users.noreply.github.com>
2022-02-08 12:31:08 -0500

    Create Day10 Syntax.py

---
+++ Day 10/Python Syntax/Day10 Syntax.py
@@ -0,0 +1,34 @@
+#Day 10 of the 30 Day Challenge
+# Introduction
+# Author: dataBio
+# Date: 08 Feb 2022
+# IDE: VSCode
+
+# import the "sys" module
+import sys
+# import the "os" module
+import os
+# import the "platform" module
+import platform
+# import the "platform" module
+import math
+# import the "re" module
+import re
+
+def main():
+
+  s1 = 'Day 10 Exercise level 1'
+  s2 = 'Day 7 Exercise level 2'
+  s3 = 'Day 7 Exercise level 3'
+  s0 =  ''
+
+  for i in range(20):
+    s0 += "--"
+
+  s = s0 + s1 + s0
+  print(s)
+  os.system("cls") # clear console
+
+    
+if __name__ == "__main__":
+  main()
