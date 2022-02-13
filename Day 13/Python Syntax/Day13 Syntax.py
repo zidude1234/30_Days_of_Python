058e710a625f5c51c794dc799768c24f8e4399e7
dataBio <46449724+zidude1234@users.noreply.github.com>
2022-02-13 15:36:21 -0500

    Create Day13 Syntax.py

---
+++ Day 13/Python Syntax/Day13 Syntax.py
@@ -0,0 +1,31 @@
+#Day Thirteen of the 30 Day Challenge
+# Introduction
+# Author: dataBio
+# Date: 13 Feb 2022
+# IDE: VSCode
+
+# import the "sys" module
+import sys
+# import the "os" module
+import os
+# import the "platform" module
+import platform
+# import the "platform" module
+import math
+# import the "re" module
+import re
+
+def main():
+
+  s1 = 'Day 13 Exercise level 1'
+
+  for i in range(20):
+    s0 += "--"
+
+  s = s0 + s1 + s0
+  print(s)
+  os.system("cls") # clear console
+
+    
+if __name__ == "__main__":
+  main()
