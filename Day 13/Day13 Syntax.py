396e89ac993514a8a8d6022888d149c5b7d71ec2
dataBio <46449724+zidude1234@users.noreply.github.com>
2022-02-14 13:18:05 -0500

    Create Day13 Syntax.py

---
+++ Day 13/Day13 Syntax.py
@@ -0,0 +1,32 @@
+----------------------------------------Day 13 Exercise level 1----------------------------------------
+Number 1
+Original List: [-4, -3, -2, -1, 0, 2, 4, 6]
+[-4, -3, -2, -1, 0]
+
+
+Number 2
+Level 1 [[[1, 2, 3]], [[4, 5, 6]], [[7, 8, 9]]]
+====================================================
+Last Level [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+Number 3
+[(0, 1, 0, 0, 0, 0, 0),
+(1, 1, 1, 1, 1, 1, 1),
+(2, 1, 2, 4, 8, 16, 32),
+(3, 1, 3, 9, 27, 81, 243),
+(4, 1, 4, 16, 64, 256, 1024),
+(5, 1, 5, 25, 125, 625, 3125),
+(6, 1, 6, 36, 216, 1296, 7776),
+(7, 1, 7, 49, 343, 2401, 16807),
+(8, 1, 8, 64, 512, 4096, 32768),
+(9, 1, 9, 81, 729, 6561, 59049),
+(10, 1, 10, 100, 1000, 10000, 100000)]
+
+Number 4
+[['FINLAND', 'FIN', 'HELSINKI'], ['SWEDEN', 'SWE', 'STOCKHOLM'], ['NORWAY', 'NOR', 'OSLO']]
+
+Number 5
+List of Dictionaries: [{'country': 'FINLAND', 'city': 'HELSINKI'}, {'country': 'SWEDEN', 'city': 'STOCKHOLM'}, {'country': 'NORWAY', 'city': 'OSLO'}]
+
+Number 6
+Combined List of Tuples: ['Asabeneh Yetayeh', 'David Smith', 'Donald Trump', 'Bill Gates']
