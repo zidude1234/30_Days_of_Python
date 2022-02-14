aec021cd0e3c5c23a3f8d425305e45a0c3b50bdc
dataBio <46449724+zidude1234@users.noreply.github.com>
2022-02-14 13:48:52 -0500

    Create Day14 Syntax.py

---
+++ Day 14/Python Syntax/Day14 Syntax.py
@@ -0,0 +1,34 @@
+#Day Fourteen of the 30 Day Challenge
+# Introduction
+# Author: dataBio
+# Date: 14 Feb 2022
+# IDE: VSCode
+
+# import the "sys" module
+import sys
+# import the "os" module
+import os
+# import the "platform" module
+import platform
+# import the "platform" module
+import math
+# import the "re" module
+import re
+
+def main():
+
+  s1 = 'Day 14 Exercise level 1'
+  s2 = 'Day 14 Exercise level 2'
+  s3 = 'Day 14 Exercise level 3'
+  s0 =  ''
+
+  for i in range(20):
+    s0 += "--"
+
+  s = s0 + s1 + s0
+  print(s)
+  os.system("cls") # clear console
+
+    
+if __name__ == "__main__":
+  main()
