34737b4a5c2c189dad5f1fc8887762094aa36f84
dataBio <46449724+zidude1234@users.noreply.github.com>
2022-02-15 16:31:25 -0500

    Create Day15 Syntax.py

---
+++ Day 15/Python Syntax/Day15 Syntax.py
@@ -0,0 +1,37 @@
+#Day Fourteen of the 30 Day Challenge
+# Introduction
+# Author: dataBio
+# Date: 14 Feb 2022
+# IDE: VSCode
+
+# import the "sys" module
+import sys
+# import the "os" module
+import os
+# import the "platform" module
+import platform
+# import the "platform" module
+import math
+# import the "re" module
+import re
+
+def main():
+
+  def p(num):
+    print(f"Number {num}")
+  
+  def pn():
+    print(f"\n")
+
+  banner = bannergreeting
+  print(banner(14,1))
+
+
+  
+
+
+  os.system("cls") # clear console
+
+    
+if __name__ == "__main__":
+  main()
