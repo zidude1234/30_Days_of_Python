7e6512d94e256ffeaaeb62acc3aa856db22b8d46
dataBio <46449724+zidude1234@users.noreply.github.com>
2022-02-18 10:59:55 -0500

    Create Day18 Syntax.py

---
+++ Day18/Python Syntax/Day18 Syntax.py
@@ -0,0 +1,49 @@
+#Day Eighteen of the 30 Day Challenge
+# Introduction
+# Author: dataBio
+# Date: 18 Feb 2022
+# IDE: VSCode
+
+# import the "sys" module
+import sys
+# import the "os" module
+import os
+# import the "platform" module
+import platform
+# import the "platform" module
+import math
+# import the "re" module
+import re
+import collections
+import datetime
+
+def main():
+  os.system("cls") # clear console
+  
+  def p(num):
+    print(f"Number {num}")
+  
+  def pn():
+    print(f"\n")
+
+  def bannergreeting(day_name,exercise_name):
+    daybanner = f'Exercise {day_name} '
+    exercisebanner = f'Level {exercise_name} '
+    front_end = f'--' * 20
+    return combinebanners(front_end,daybanner,exercisebanner)
+  
+  def combinebanners(s0,s1,s2):
+    return s0 + s1 + s2 + s0
+
+  banner = bannergreeting
+  print(banner(18,1))
+
+
+  
+
+
+  
+
+    
+if __name__ == "__main__":
+  main()
