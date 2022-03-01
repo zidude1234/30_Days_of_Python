3fd1f6de247b305e0ab806fe793d12a606e16878
dataBio <46449724+zidude1234@users.noreply.github.com>
2022-03-01 07:04:52 -0500

    Create Day22 Syntax.py

---
+++ Day 22/Python Syntax/Day22 Syntax.py
@@ -0,0 +1,60 @@
+#Day TwentyTwo of the 30 Day Challenge
+# Introduction
+# Author: dataBio
+# Date: 01 Mar 2022
+# IDE: VSCode
+
+# import the "sys" module
+import sys
+# import the "os" module
+import os
+# import the "platform" module
+import platform
+# import the "platform" module
+import math
+# import the "re" module
+import re
+import collections
+import datetime
+from itertools import chain
+import json
+import csv
+import numpy as np
+import webbrowser
+import matplotlib as plt
+import requests
+import statistics
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+def main():
+  os.system("cls") # clear console
+  
+  def p(num):
+    print(f"Number {num}")
+
+  def pn(num = 1):
+    print(f"\n" * num)
+
+  def bannergreeting(day_name,exercise_name):
+    daybanner = f'Exercise {day_name} '
+    exercisebanner = f'Level {exercise_name} '
+    front_end = f'--' * 20
+    return combinebanners(front_end,daybanner,exercisebanner)
+  
+  def combinebanners(s0,s1,s2):
+    return s0 + s1 + s2 + s0
+
+  banner = bannergreeting
+  print(banner(22,1))
+
+
+  
+
+
+  
+
+    
+if __name__ == "__main__":
+  main()
